03871674e73a8b517f017904970eb8774ff668d7
Mike Paterson <michael.paterson@hudl.com>
2016-09-12 12:40:09 -0400

    BETA-130 Initial implementation of datadog cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures role_datadog'
 long_description 'Installs/Configures role_datadog'
-version '0.1.0'
+version '0.1.1'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+encrypted = Chef::EncryptedDataBagItem.load('datadog', 'datadog-encrypted')
+
+node.default['datadog']['api_key'] = encrypted['api_key']
+
+include_recipe 'datadog::dd-agent'
+include_recipe 'datadog::dd-handler'
