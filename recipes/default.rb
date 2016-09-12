f64767f51841cff92ae9b9f5134b0b41b1703303
Mike Paterson <michael.paterson@hudl.com>
2016-09-12 14:13:06 -0400

    Added application_key databag item

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,7 @@
 encrypted = Chef::EncryptedDataBagItem.load('datadog', 'datadog-encrypted')
 
 node.default['datadog']['api_key'] = encrypted['api_key']
+node.default['datadog']['application_key'] = encrypted['application_key']
 
 include_recipe 'datadog::dd-agent'
 include_recipe 'datadog::dd-handler'
