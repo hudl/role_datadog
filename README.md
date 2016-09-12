f683345589c04ab5359ca9bf35fc9b6d868a299e
Mike Paterson <michael.paterson@hudl.com>
2016-09-12 11:40:34 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# role_datadog
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: role_datadog
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
